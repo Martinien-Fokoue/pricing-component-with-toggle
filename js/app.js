bc5910cb43a6db2cdd74085b33be71817ff5a199
Martinien Fokoue <martinienfokoue@gmail.com>
2020-12-16 20:15:37 +0100

    completed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 var toggle = document.querySelector('.toggle'),
-    round = toggle.querySelector('.round');
+    round = toggle.querySelector('.round'),
+    price = document.querySelectorAll('.price'),
+    prices_annual = ['&dollar;199.99','&dollar;249.99','&dollar;399.99'],
+    prices_montly = ['&dollar;19.99','&dollar;24.99','&dollar;39.99'],
+    annual = false; 
 
 toggle.addEventListener('click', (e)=>{
-   
-    console.log(round.classList);
     round.classList.toggle('active');
-    console.log(round.classList);
+    annual = !annual;
 
+    for(let i = 0; i < price.length; i++){
+        if(annual === true){
+            price[i].innerHTML = prices_annual[i];
+        }else {
+            price[i].innerHTML = prices_montly[i];
+        }
+    } 
 });
